9104db87497f3367563eb96bfc9859de16852c36
mariya <mgrylyuk@gmail.com>
2019-03-19 15:40:02 +0200

    Update GameController spec

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -65,6 +65,18 @@ RSpec.describe GamesController, type: :controller do
       expect(flash.empty?).to be_truthy # удачный ответ не заполняет flash
     end
 
+    it 'answers wrong' do
+
+      wrong = %w[a b c d].find { |a| a != game_w_questions.current_game_question.correct_answer_key }
+      put :answer, id: game_w_questions.id, letter: wrong
+      game = assigns(:game)
+
+      expect(game.finished?).to be true
+      expect(game.current_level).to be_zero
+      expect(response).to redirect_to(user_path(user))
+      expect(flash[:alert]).to be
+    end
+
     it '#show alien game' do
       # создаем новую игру, юзер не прописан, будет создан фабрикой новый
       alien_game = FactoryGirl.create(:game_with_questions)
